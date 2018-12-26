11b7255dd3fff349928be13d32a371612e0a98cb
alex <alex_is_ok@yahoo.com>
2018-12-26 21:59:14 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/payment'
-  get 'static_pages/delivery'
-  get 'static_pages/about'
+  root 'static_pages#home', as: 'root'
+
+  get 'payment', to: 'static_pages#payment', as: 'payment'
+  get 'delivery', to: 'static_pages#delivery', as: 'delivery'
+  get 'about', to: 'static_pages#about', as: 'about'
+
   resources :stuffs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
