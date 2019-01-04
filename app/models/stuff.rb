432c2e5109c1756e815116b3cb0bb0feffde09fa
alex <alex_is_ok@yahoo.com>
2019-01-05 01:31:38 +0200

    some fix added

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,3 +1,6 @@
 class Stuff < ApplicationRecord
   acts_as_taggable_on :tags
+
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :short_description, presence: true, length: { maximum: 120 }
 end
